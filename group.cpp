4dfb40499630fbd6c87df08b37cc0f9a537ed8c9
thwo93 <cos38zb@outlook.com>
2024-06-21 11:24:42 +0800

    Modify group.cpp

--- group.cpp
+++ group.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Activity church take life.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
