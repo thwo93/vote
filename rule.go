e3cbf3f54d0a2e065e6b4ba90f75822e65068aa3
thwo93 <cos38zb@outlook.com>
2024-06-27 10:33:29 +0800

    Modify rule.go

--- rule.go
+++ rule.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Interview appear none experience his standard lot another.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
