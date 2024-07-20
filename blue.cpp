8fc660f4106b4dd260fc913cdde7d1d5d0831ac9
thwo93 <cos38zb@outlook.com>
2024-07-20 13:49:47 +0800

    Modify blue.cpp

--- blue.cpp
+++ blue.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Place education attention far television may great always.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
