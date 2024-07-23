cefa9dded1d52f7961a9556fab3efcbdb36aa5ed
thwo93 <cos38zb@outlook.com>
2024-07-23 13:10:37 +0800

    Modify bill.cpp

--- bill.cpp
+++ bill.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Company color return thus role everyone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
