9f305e91dff25fcf19a55ba15323a39a40983e71
thwo93 <cos38zb@outlook.com>
2024-07-30 14:26:23 +0800

    Modify contain.c

--- contain.c
+++ contain.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Case improve simply century second science.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
