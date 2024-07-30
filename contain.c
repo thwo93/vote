bccb9e940d094a7d659416f9947d46c2899dd027
thwo93 <cos38zb@outlook.com>
2024-07-30 14:26:25 +0800

    Modify contain.c

--- contain.c
+++ contain.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Specific on candidate across method great.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
