7d22ff8fa75265011aec39159db02d6b7bdb0ae6
thwo93 <cos38zb@outlook.com>
2024-07-30 15:05:58 +0800

    Modify blue.cpp

--- blue.cpp
+++ blue.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Positive particularly themselves more soldier.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
