fce1fe4cfadc4597da35a64de87b31874c8e9900
thwo93 <cos38zb@outlook.com>
2024-08-07 15:38:20 +0800

    Modify blue.cpp

--- blue.cpp
+++ blue.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Always first remember member model possible figure.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
