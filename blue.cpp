53af642d266ae827253c0457cdab6d85f4f9bdc1
thwo93 <cos38zb@outlook.com>
2024-08-21 19:41:22 +0800

    Modify blue.cpp

--- blue.cpp
+++ blue.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Suggest table mean like east development.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
