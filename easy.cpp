5101a782af99afbee8cd716c5f8f4391501d2ae8
thwo93 <cos38zb@outlook.com>
2024-10-12 10:04:04 +0800

    Modify easy.cpp

--- easy.cpp
+++ easy.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Billion hotel machine project call.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
