07ae932fbfe8df069af74a7adea9438df26c8bbf
thwo93 <cos38zb@outlook.com>
2024-10-16 11:16:13 +0800

    Modify ready.cpp

--- ready.cpp
+++ ready.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Affect child meet personal.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
