ffb84266b3fe1c8ed1f78c970fc0558a2450bd03
thwo93 <cos38zb@outlook.com>
2024-10-17 10:36:39 +0800

    Modify ready.cpp

--- ready.cpp
+++ ready.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Trade avoid save spring.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
