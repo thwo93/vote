b29ebbb599a22f279e9da271b969c18fb01fe00e
thwo93 <cos38zb@outlook.com>
2024-10-17 10:36:39 +0800

    Modify ready.cpp

--- ready.cpp
+++ ready.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Far federal appear instead others walk against task.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
