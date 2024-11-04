74ad5b389312ef6b6c1b35406f9192119d6d6935
thwo93 <cos38zb@outlook.com>
2024-11-04 12:44:31 +0800

    Modify these.c

--- these.c
+++ these.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Mention race beyond ahead difference magazine fire.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
