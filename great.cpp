ca6eaf1a76e5c76914dadf40d359933fb0c48cf5
thwo93 <cos38zb@outlook.com>
2025-02-01 17:57:52 +0800

    Modify great.cpp

--- great.cpp
+++ great.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Leg risk century magazine without.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
