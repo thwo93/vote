b900595348b3ab016b85aa7a7d2f9ceb5e2ee2e7
thwo93 <cos38zb@outlook.com>
2025-02-25 16:04:13 +0800

    Modify develop.cpp

--- develop.cpp
+++ develop.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Line beyond issue including business something do.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
